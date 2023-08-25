1c43c23272fa9c3fddc92f78babb488daa4f1366
mldkyt <mldkyt@protonmail.com>
2023-08-25 10:10:59 +0200

    Finish dark mode

--- app/components/MapLoading.tsx
+++ app/components/MapLoading.tsx
@@ -1,23 +1,21 @@
-import {motion} from "framer-motion";
+import { ConfigProvider, Spin, Typography } from "antd";
 
 export default function MapLoading() {
-  return <>
-    <motion.img
-      animate={{
-        transform: [
-          'translate(-50%, -50%) rotate(0deg)',
-          'translate(-50%, -50%) rotate(360deg)'
-        ]
-      }}
-      transition={{repeat: Infinity, duration: 1, ease: 'easeInOut'}}
-      style={{
-        position: 'fixed',
-        left: '50%',
-        top: '50%',
-        transform: 'translate(-50%, -50%)'
-      }}
-      src='/astolfo loading.jpg'
-      height={128}>
-    </motion.img>
-  </>
+  return <div style={{
+    position: 'fixed',
+    left: '0',
+    top: '0',
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: '#000000'
+  }}>
+    <div style={{
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}>
+      <Spin size="large" />
+    </div>
+  </div>
 }
