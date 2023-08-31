2c3da254efdc9d0cd8136e683ff137c57becf9f5
mldkyt <mldkyt@protonmail.com>
2023-08-31 18:49:02 +0200

    Admin page

---
+++ app/routes/api.getusers.ts
@@ -0,0 +1,16 @@
+import { LoaderFunction } from "@remix-run/node";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const auth = request.headers.get("Authorization") ?? '';
+  const users = await (
+    await fetch(`${process.env.FIREBASE}/users.json`)
+  ).json();
+  const adminUser = users.find((user: any) => user.username === "astolfo");
+  if (!adminUser) return {};
+  if (!adminUser.tokens) adminUser.tokens = [];
+  if (!adminUser.tokens.includes(auth)) return {};
+
+  return {
+    users,
+  };
+};
